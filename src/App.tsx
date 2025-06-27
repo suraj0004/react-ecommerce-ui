63f9e0a2123540a041e1b91e8a45534912241708
suraj <surajupadhyaa@gmail.com>
2025-06-27 12:14:47 +0530

    refactor: changes in router for github pages hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
@@ -9,7 +9,8 @@ import ProductDetail from "./pages/product-detail";
 import Cart from "./pages/cart";
 import NotFound from "./pages/not-found";
 
-function Router() {
+// Custom Router component using Wouter
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -24,8 +25,10 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Router />
+        <WouterRouter base="/react-ecommerce-ui">
+          <Toaster />
+          <AppRouter />
+        </WouterRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
